e9721021813c0d2c79fbeacb1ad15380fa977584
leikt <leikt.solreihin@gmail.com>
2021-10-19 15:02:41 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="acpoa-plugin",
+    version="0.0.1",
+    author="Leikt",
+    author_email="leikt.solreihin@gmail.com",
+    description="ACPOA plugin creation helper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    package_data={'': ['default/*']}
+)
